83ff9b3d56420aed2395c6e4652f57c694650d6e
a-vasin <a-vasin@yandex-team.ru>
2019-12-29 16:31:26 +0300

    - Finish unit tests

--- src/test/java/MoneyTransferManagerTest.java
+++ src/test/java/MoneyTransferManagerTest.java
@@ -8,9 +8,10 @@ import org.junit.jupiter.api.Test;
 public class MoneyTransferManagerTest {
 
     private final int ID = 1;
-    private final int TO_ID = 2;
+    private final int ANOTHER_ID = 2;
     private final int DEPOSIT_AMOUNT = 100;
     private final int TRANSFER_AMOUNT = 10;
+    private final int OPERATIONS_COUNT = 10_000_000;
 
     private MoneyTransferManager transferManager;
 
@@ -51,27 +52,148 @@ public class MoneyTransferManagerTest {
         Assertions.assertEquals(0, balance);
     }
 
+    private static Runnable wrapException(ThrowingRunnable throwingRunnable) {
+        return () -> {
+            try {
+                throwingRunnable.run();
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+        };
+    }
+
     // assume TRANSFER_AMOUNT is even
     @Test
     public void testTransfer() throws MissingAccountException, NotEnoughMoneyException {
         transferManager.createAccount(ID);
         transferManager.deposit(ID, DEPOSIT_AMOUNT);
 
-        transferManager.createAccount(TO_ID);
-        int fromBalance = transferManager.transfer(ID, TO_ID, TRANSFER_AMOUNT);
+        transferManager.createAccount(ANOTHER_ID);
+        int fromBalance = transferManager.transfer(ID, ANOTHER_ID, TRANSFER_AMOUNT);
         Assertions.assertEquals(DEPOSIT_AMOUNT - TRANSFER_AMOUNT, fromBalance);
 
         fromBalance = transferManager.getBalance(ID);
         Assertions.assertEquals(DEPOSIT_AMOUNT - TRANSFER_AMOUNT, fromBalance);
 
-        int toBalance = transferManager.getBalance(TO_ID);
+        int toBalance = transferManager.getBalance(ANOTHER_ID);
         Assertions.assertEquals(TRANSFER_AMOUNT, toBalance);
 
         // transfer half back
-        toBalance = transferManager.transfer(TO_ID, ID, TRANSFER_AMOUNT / 2);
+        toBalance = transferManager.transfer(ANOTHER_ID, ID, TRANSFER_AMOUNT / 2);
         Assertions.assertEquals(TRANSFER_AMOUNT / 2, toBalance);
 
         fromBalance = transferManager.getBalance(ID);
         Assertions.assertEquals(DEPOSIT_AMOUNT - TRANSFER_AMOUNT / 2, fromBalance);
+    }
+
+    @Test
+    public void testDoubleCreation() throws MissingAccountException {
+        boolean created = transferManager.createAccount(ID);
+        Assertions.assertTrue(created);
+
+        transferManager.deposit(ID, DEPOSIT_AMOUNT);
+
+        created = transferManager.createAccount(ID);
+        Assertions.assertFalse(created);
+
+        int balance = transferManager.getBalance(ID);
+        Assertions.assertEquals(DEPOSIT_AMOUNT, balance);
+    }
+
+    @Test
+    public void testDepositMissingAccount() {
+        Assertions.assertThrows(
+                MissingAccountException.class,
+                () -> transferManager.deposit(ID, DEPOSIT_AMOUNT)
+        );
+    }
+
+    @Test
+    public void testWithdrawMissingAccount() {
+        Assertions.assertThrows(
+                MissingAccountException.class,
+                () -> transferManager.withdraw(ID, DEPOSIT_AMOUNT)
+        );
+    }
+
+    @Test
+    public void testTransferMissingAccountFrom() {
+        Assertions.assertThrows(
+                MissingAccountException.class,
+                () -> transferManager.transfer(ID, ANOTHER_ID, DEPOSIT_AMOUNT)
+        );
+    }
+
+    @Test
+    public void testTransferMissingAccountTo() {
+        transferManager.createAccount(ID);
+
+        Assertions.assertThrows(
+                MissingAccountException.class,
+                () -> transferManager.transfer(ID, ANOTHER_ID, DEPOSIT_AMOUNT)
+        );
+    }
+
+    @Test
+    public void testWithdrawNotEnoughMoney() {
+        transferManager.createAccount(ID);
+        Assertions.assertThrows(
+                NotEnoughMoneyException.class,
+                () -> transferManager.withdraw(ID, DEPOSIT_AMOUNT)
+        );
+    }
+
+    @Test
+    public void testTransferNotEnoughMoney() {
+        transferManager.createAccount(ID);
+        transferManager.createAccount(ANOTHER_ID);
+
+        Assertions.assertThrows(
+                NotEnoughMoneyException.class,
+                () -> transferManager.transfer(ID, ANOTHER_ID, DEPOSIT_AMOUNT)
+        );
+    }
+
+    @Test
+    public void testAsyncActions() throws MissingAccountException, InterruptedException {
+        transferManager.createAccount(ID);
+        transferManager.createAccount(ANOTHER_ID);
+
+        transferManager.deposit(ID, OPERATIONS_COUNT);
+        transferManager.deposit(ANOTHER_ID, OPERATIONS_COUNT);
+
+        Thread depositThread = new Thread(wrapException(() -> {
+            for (int i = 0; i < OPERATIONS_COUNT; ++i) {
+                transferManager.deposit(ID, 1);
+            }
+        }));
+
+        Thread withdrawThread = new Thread(wrapException(() -> {
+            for (int i = 0; i < OPERATIONS_COUNT; ++i) {
+                transferManager.withdraw(ID, 1);
+            }
+        }));
+
+        Thread transferThread = new Thread(wrapException(() -> {
+            for (int i = 0; i < OPERATIONS_COUNT; ++i) {
+                transferManager.transfer(ANOTHER_ID, ID, 1);
+            }
+        }));
+
+        depositThread.start();
+        withdrawThread.start();
+        transferThread.start();
+
+        depositThread.join();
+        withdrawThread.join();
+        transferThread.join();
+
+        int balance = transferManager.getBalance(ID);
+        Assertions.assertEquals(2 * OPERATIONS_COUNT, balance);
+    }
+
+    @FunctionalInterface
+    private interface ThrowingRunnable {
+        void run() throws Exception;
     }
 }
